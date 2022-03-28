da64524fe021c152e6fc0d7ddca7d7fde66a5552
Jeon Sangmin <45033215+sangmandu@users.noreply.github.com>
2022-03-29 02:14:02 +0900

    [Silver III] Title: 소수 구하기, Time: 328 ms, Memory: 44548 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+m, n = map(int, input().split())
+
+sieve = [False] * 2 + [True] * (n-1)
+for i in range(2, int(n ** 0.5)+1):
+    if sieve[i]:
+        for j in range(i+i, n+1, i):
+            sieve[j] = False
+print(*[i for i in range(m, n+1) if sieve[i]], sep='\n')
