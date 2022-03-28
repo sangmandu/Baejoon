87fe6cce2d6396720cc1d91beeb735d17dfb652c
Jeon Sangmin <45033215+sangmandu@users.noreply.github.com>
2022-03-29 02:11:38 +0900

    [Silver I] Title: 호텔, Time: 80 ms, Memory: 29596 KB -BaekjoonHub

---
+++ 백준/Silver/1106. 호텔/README.md
@@ -0,0 +1,12 @@
+# [Silver I] 호텔 - 1106 
+
+[문제 링크](https://www.acmicpc.net/problem/1106) 
+
+### 성능 요약
+
+메모리: 29596 KB, 시간: 80 ms
+
+### 분류
+
+다이나믹 프로그래밍(dp), 배낭 문제(knapsack)
+
---
+++ 백준/Silver/1106. 호텔/호텔.py
@@ -0,0 +1,12 @@
+from sys import stdin
+
+def solution(target, cities):
+    graph = [[0] + [100000] * target  for i in range(len(cities)+1)]
+    for idx, b in enumerate(cities):
+        for i in range(1, target+1):
+                graph[idx+1][i] = min(graph[idx][i], graph[idx+1][i-b[1] if i-b[1] >=0 else 0]+b[0])
+    print(graph[len(cities)][target])
+
+target, city = map(int, stdin.readline().split())
+cities = [list(map(int, stdin.readline().split())) for _ in range(city)]
+solution(target, cities)
