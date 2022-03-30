f7a411eac609af3367aa02ba436b18b98b0c540d
Jeon Sangmin <45033215+sangmandu@users.noreply.github.com>
2022-03-30 15:07:17 +0900

    [Gold V] Title: 공유기 설치, Time: 796 ms, Memory: 40152 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+N, C = map(int, input().split())
+lst = sorted([int(input()) for _ in range(N)])
+
+left, right = 0, max(lst)
+while left <= right:
+    mid = (left + right) // 2
+    cnt, pre = C-1, lst[0]
+    for item in lst[1:]:
+        if item - pre >= mid:
+            pre = item
+            cnt -= 1
+            if not cnt:
+                break
+    if cnt:
+        right = mid - 1
+    else:
+        left, ans = mid + 1, mid
+
+print(ans)
