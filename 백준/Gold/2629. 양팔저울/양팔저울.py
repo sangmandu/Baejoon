adf1f8193eae8fb318fd7d1d56b786a7b41d7339
Jeon Sangmin <45033215+sangmandu@users.noreply.github.com>
2022-06-07 18:04:50 +0900

    [Gold III] Title: 양팔저울, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.py
+++ 백준/Gold/2629. 양팔저울/양팔저울.py
@@ -1,19 +1,13 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-weight = list(map(int, input().split()))
-max_weight = 40001
-
-dp = [[0] * max_weight for _ in range(N+1)]
-for idx, w in enumerate(weight, 1):
-    for jdx in range(idx, N+1):
-        dp[jdx][w] = 1
-    for x in range(max_weight):
-        if dp[idx-1][x]:
-            for jdx in range(idx, N+1):
-                dp[jdx][abs(w-x)] = dp[jdx][w+x] = 1
+input()
+dp = set([0])
+for w in list(map(int, input().split())):
+    tmp = set()
+    for d in dp:
+        tmp.update([d, d+w, d-w])
+    dp = tmp
 
 input()
-YorN = ['N', 'Y']
-print(' '.join(list(map(lambda x: YorN[dp[-1][int(x)]], input().split()))))
+print(' '.join(map(lambda x: 'Y' if int(x) in dp else 'N', input().split())))
