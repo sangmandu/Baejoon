aa49a61ab7a75ee5c26b393ebfc3c1adfc1b5891
Jeon Sangmin <45033215+sangmandu@users.noreply.github.com>
2022-07-05 14:37:29 +0900

    [Gold III] Title: 나머지 합, Time: 620 ms, Memory: 147164 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.py
+++ 백준/Gold/10986. 나머지 합/나머지 합.py
@@ -2,12 +2,13 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-lst = list(map(int, input().split())) + [0]
+lst = list(map(int, input().split()))
 
+acc = 0
 mod = [0] * M
-for idx in range(N):
-    lst[idx] += lst[idx-1]
-    mod[lst[idx] % M] += 1
+for val in lst:
+    acc += val
+    mod[acc % M] += 1
 
 ans = mod[0]
 for m in mod:
